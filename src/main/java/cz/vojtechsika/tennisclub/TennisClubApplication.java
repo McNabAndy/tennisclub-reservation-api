b7ea175a7efbf464bcc8081aecf0d0a816810e31
Vojtěch Šika <vojtechsika@gmail.com>
2025-05-19 23:16:12 +0200

    Configure H2 database with console

--- src/main/java/cz/vojtechsika/tennisclub/TennisClubApplication.java
+++ src/main/java/cz/vojtechsika/tennisclub/TennisClubApplication.java
@@ -2,12 +2,17 @@ package cz.vojtechsika.tennisclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class TennisClubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TennisClubApplication.class, args);
+
+		System.out.println("Tennis club application started");
 	}
+
+
 
 }

ae23646447e835d43bc07ab3b34a9224647aa032
Vojtěch Šika <vojtechsika@gmail.com>
2025-05-23 09:16:44 +0200

    Configure initial data loading with data.sql and defer execution

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO surface_type (name, minute_price, deleted) VALUES ( 'Clay', 1.5, false);
+INSERT INTO surface_type (name, minute_price, deleted) VALUES ( 'Grass', 2.0, false);
+INSERT INTO court (court_number, surface_type_id, deleted) VALUES ( 101, 1, false);
+INSERT INTO court (court_number, surface_type_id, deleted) VALUES ( 102, 2, false);
